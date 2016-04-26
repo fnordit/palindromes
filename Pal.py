bb7483d3c013530aadc732ebb8c920caee2a74a7
Sean <fnordit@gmail.com>
2016-04-26 01:08:47 -0700

    Switched to printing as we go (memory efficient)

--- Pal.py
+++ Pal.py
@@ -1,13 +1,15 @@
 from Trie import *
+from re import *
 
 def is_palindrome(word):
     return word == word[::-1]
 
 def get_palindromes(root, remainder, front, depth):
     if depth <= 0:
-        return []
+        return
     if remainder == "":
-        return [root]
+        print root
+        return
     ret = []
     if front:
         # [                     root | remainder]
@@ -17,14 +19,14 @@ def get_palindromes(root, remainder, front, depth):
         #print(candidates)
         for danc in candidates:
             newroot = danc[::-1] + remainder[::-1] + " " + root
-            ret += get_palindromes(newroot, danc, False, depth-1)
+            get_palindromes(newroot, danc, False, depth-1)
         # [      root | remainder   ]
         # [mer + root + rem | ainder]
         for i in range(len(remainder)+1):
             if backward.is_word(remainder[0:i]):
                 newroot = remainder[0:i][::-1] + " " + root 
                 newrem = remainder[i:]
-                ret += get_palindromes(newroot, newrem, True, depth-1)
+                get_palindromes(newroot, newrem, True, depth-1)
     else:
         # [remainder | root]
         # [remainder + root + redniamder | newrem]
@@ -33,15 +35,15 @@ def get_palindromes(root, remainder, front, depth):
         #print(candidates)
         for cand in candidates:
             newroot = root + " " + remainder + cand
-            ret += get_palindromes(newroot, cand, True, depth-1)
+            get_palindromes(newroot, cand, True, depth-1)
         # [remainder    | root      ]
         # [remain | der + root + red]
         for i in range(1,len(remainder)+1):
             if forward.is_word(remainder[0:i]):
                 newroot = root + " " + remainder[0:i]
                 newrem = remainder[i:]
-                ret += get_palindromes(newroot, newrem, False, depth-1)
-    return ret
+                get_palindromes(newroot, newrem, False, depth-1)
+    return
 
 
 
@@ -50,15 +52,14 @@ dictionary = open('/usr/share/cracklib/cracklib-small', 'r')
 forward = Trie()
 backward = Trie()
 for word in dictionary:
+    if 
     forward.insert(word.strip())
     backward.insert(word[::-1].strip())
 
 for word in forward.find_words():
     for i in range(len(word)):
         if is_palindrome(word[0:i]):
-            for pal in get_palindromes(word, word[i:], True, 7):
-                print(pal)
+            get_palindromes(word, word[i:], True, 7)
     for i in reversed(range(len(word))):
         if is_palindrome(word[i:]):
-            for pal in get_palindromes(word, word[0:i], False, 7):
-                print(pal)
+            get_palindromes(word, word[0:i], False, 7)
