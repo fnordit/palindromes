e2eca4400772dd043b5bf11b320fba32e493b876
Sean <fnordit@gmail.com>
2016-05-03 21:37:11 -0700

    Cleaned up debugging lines, added reverse helper function

--- Pal.py
+++ Pal.py
@@ -3,8 +3,11 @@ import re
 import argparse
 import sys
 
+def reverse(string):
+    return string[::-1]
+
 def is_palindrome(word):
-    return word == word[::-1]
+    return word == reverse(word)
 
 def get_palindromes(root, remainder, front, depth):
     if depth <= 0:
@@ -12,29 +15,24 @@ def get_palindromes(root, remainder, front, depth):
     if remainder == "":
         print root
         return
-    ret = []
     if front:
         # [                     root | remainder]
         # [merwen | redniamer + root + remainder]
         candidates = backward.find_from_pref(remainder)
-        #print(str(depth) + "FRONT\tRoot: " + root + "\tRemainder: " + remainder)
-        #print(candidates)
         for danc in candidates:
-            newroot = danc[::-1] + remainder[::-1] + " " + root
+            newroot = reverse(danc) + reverse(remainder) + " " + root
             get_palindromes(newroot, danc, False, depth-1)
         # [      root | remainder   ]
         # [mer + root + rem | ainder]
         for i in range(len(remainder)+1):
             if backward.is_word(remainder[0:i]):
-                newroot = remainder[0:i][::-1] + " " + root 
+                newroot = reverse(remainder[0:i]) + " " + root 
                 newrem = remainder[i:]
                 get_palindromes(newroot, newrem, True, depth-1)
     else:
         # [remainder | root]
         # [remainder + root + redniamder | newrem]
         candidates = forward.find_from_pref(remainder)
-        #print(str(depth) + "BACK\tRoot: " + root + "\tRemainder: " + remainder)
-        #print(candidates)
         for cand in candidates:
             newroot = root + " " + remainder + cand
             get_palindromes(newroot, cand, True, depth-1)
@@ -49,7 +47,7 @@ def get_palindromes(root, remainder, front, depth):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dictionary", help="dictionary file to read, line separated")
-parser.add_argument("length", help="dictionary file to read, line separated", type=int)
+parser.add_argument("length", help="maximum palindrome length", type=int)
 args = parser.parse_args()
 try:
     dictionary = open(args.dictionary, 'r')
@@ -69,4 +67,4 @@ for word in forward.find_words():
             get_palindromes(word, word[i:], True, args.length)
     for i in reversed(range(len(word))):
         if is_palindrome(word[i:]):
-            get_palindromes(word, word[0:i][::-1], False, args.length)
+            get_palindromes(word, reverse(word[0:i]), False, args.length)
